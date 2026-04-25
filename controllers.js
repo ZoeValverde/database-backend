ed53dbaf7a8a0335d8da29b5aa50a89d7ad1eea8
zoevalverde <zoevalverde00@gmail.com>
2026-04-25 17:55:36 -0300

    add: controllers with CRUD functions

---
+++ controllers.js
@@ -0,0 +1,17 @@
+const getUser = () => {
+    console.log("leyendo usuario")
+}
+
+const createUser = () => {
+    console.log("usuario creado")
+}
+
+const updateUser = () => {
+    console.log("usuario actualizado")
+}
+
+const deleteUser = () => {
+    console.log("usuario eliminado")
+}
+
+export{getUser, updateUser, deleteUser, createUser}
---
+++ index.js
@@ -0,0 +1,29 @@
+import { getUser, updateUser, createUser, deleteUser } from "./controllers.js"
+
+const argv = process.argv
+const params = argv.slice(2)
+const operacion = params[0]
+let resultado
+
+switch (operacion) {
+    case "get":
+        resultado = getUser()
+        break
+    case "add":
+        resultado =createUser()
+        break
+    case "update":
+        resultado = updateUser()
+        break
+    case "delete":
+        resultado = deleteUser()
+        break
+    default:
+        resultado= "operacion invalida"
+}
+
+const main = () => {
+  console.log(resultado)
+}
+
+main()

ec1d8f34727c9aeeede3b5b9808aa7186cb6501d
zoevalverde <zoevalverde00@gmail.com>
2026-04-29 18:10:02 -0300

    add: catch error DB conection, delete deleteAll function and add descriptions on project

--- controllers.js
+++ controllers.js
@@ -1,28 +1,36 @@
+//se importa la conexión de la base de datos desde el config.js
 import { db } from "./config.js"
-const { database } = db
 
+// la validación del usuario 
 let userInvalidation = false
+//expresión que permite unicamente letras en username
 const usernameRegex = /^[a-zA-Z]+$/
 
+//En esta función lee los usuarios de la base de datos y lo muestra en la consola
 const getUsers = async () => {
 
+//Se crea un query con SQL para usar SELECT y mostrar en la terminal los usuarios guardados en la base de datos
     const q = `SELECT * from users `
     const [response] = await db.query(q)
+
+    //En caso de no haber ningún usuario aparece el error
     if (response.length === 0) {
-        console.log("No se encuentran usuarios")
+        console.log("No se están encontrando usuarios")
         return
     }
     return response
 }
-
+//función que crea el usuario y lo persiste en la base de datos, tomando el nombre de usuario, email y la contraseña para crear el mismo
 const createUser = async (username, email, password) => {
+
+//control de errores al crear el usuario
     if (!username || !email || !password) {
         console.log("Se necesita nombre de usuario, email y constraseña para poder crear un usuario")
         return;
     }
 
     if (!usernameRegex.test(username)) {
-        console.log("El username solo puede contener letras.")
+        console.log("El username solo puede contener letras." )
         userInvalidation = true;
     }
 
@@ -39,11 +47,12 @@ const createUser = async (username, email, password) => {
         console.log("el password debe tener minimo 4 máximo 10 carácteres")
         userInvalidation = true
     }
-
+//En caso de que userInvalidation sea positivo se retorna, evita ejecutar el Query
     if (userInvalidation) {
         return
     }
-    
+
+//Se crea un Query para crear en users un objeto con los siguientes datos
     const q = `INSERT INTO users (id, username, email, password) VALUES(?,?,?,?)`
 
     const [response] = await db.query(q, [crypto.randomUUID(), username, email, password])
@@ -53,8 +62,10 @@ const createUser = async (username, email, password) => {
     }
 }
 
-
+//En esta funcion actualiza el usuario que yo prefiera, escribiendo los datos a actualizar y el id 
 const updateUser = async (newUsername, newEmail, newPassword, id) => {
+
+    //control de errores con lo necesario que los datos de usuario sean validos
     if (!newUsername || !newEmail || !newPassword || !id) {
         console.log("Se necesita nombre de usuario, email, constraseña y el id para poder actualizar el usuario")
         return
@@ -86,33 +97,35 @@ const updateUser = async (newUsername, newEmail, newPassword, id) => {
    const q = `UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?`
     const [response] = await db.query(q, [newUsername, newEmail, newPassword, id])
 
-
+//Este funciona para ver si se encuentra un id como el que el cliente 
     if (response.affectedRows==0) {
         console.log("no se encontró el id, pruebe otro")
-        return
-        
+        return   
     }
+
+//Si no hubo cambios es porque se encontraron los mismos datos, 
         if (!response.info.includes(`Changed: 1`)) {
             console.log("Los datos son los mismos, intente con otro")
             return
-
     }
 
     if (response.serverStatus===2 ) {
         return "usuario actualizado"
     }
-
 }
-
+//Función para eliminar un usuario, usando un id como parametro
 const deleteUser = async (id) => {
+
+//un if en caso de que no haya ningún id como argumento
     if (!id) {
-    console.log("Se necesita un id para poder eliminar el usuario")}
+        console.log("Se necesita un id para poder eliminar el usuario")
+        return
+    }
 
   const q = `DELETE from users WHERE id = ?;`
     const [response] = await db.query(q, [id]);
 
-    console.log(response)
-
+//Lo mismo que en updateUser, si no hubo fila afectada es porque no se encontró un id igual al que el cliente puso como input.
     if (response.affectedRows == 0) {
         console.log("No se encuentra ese Id")
         return
@@ -121,17 +134,8 @@ const deleteUser = async (id) => {
     if (response.serverStatus === 2) {
         return "usuario eliminado"
     }
+    console.log(response)
     
 }
-
-const deleteAll = async () => {
-     const q = `DELETE from users`
-    const [response] = await db.query(q);
-    
-    if (response.serverStatus === 2) {
-        console.log("usuarios eliminado")
-        return
-    }
-}
-
-export{getUsers, createUser, updateUser, deleteUser, deleteAll}
+//Se exporta las funciones para que se puedan usar en el index
+export{getUsers, createUser, updateUser, deleteUser}
--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-import { getUsers, updateUser, createUser, deleteUser, deleteAll } from "./controllers.js"
+//Se importa las funciones CRUD de controllers.js
+import { getUsers, updateUser, createUser, deleteUser } from "./controllers.js"
+
+//Las siguientes constantes capturan los argumentos y usa el metodo slice para que el primero (posición 0) sea la operación
 
 const argv = process.argv
 const params = argv.slice(2)
 const operacion = params[0]
+//Se crea un let para que al finalizar una funcion se ejecute un "resultado"
 let resultado
 
-
-
-switch (operacion) {
-    case "get":
-        resultado = await getUsers()
-        break
-    case "add":
-        resultado= await createUser(params[1], params[2], params[3])
-        break
-    case "update":
-        resultado = await updateUser(params[1], params[2], params[3], params[4])
-        break
-    case "delete":
-        resultado = await deleteUser(params[1])
-        break
-    case "deleteAll":
-        resultado = await deleteAll()
-        break
-    default:
-        resultado= "Se necesita una operación valida, intenta con add, get, update, delete o deleteAll"
+//El siguiente Try and Catch intenta conectar las funciones de la base de datos
+try {
+    switch (operacion) {
+        case "get":
+            resultado = await getUsers()
+            break
+        case "add":
+            resultado = await createUser(params[1], params[2], params[3])
+            break
+        case "update":
+            resultado = await updateUser(params[1], params[2], params[3], params[4])
+            break
+        case "delete":
+            resultado = await deleteUser(params[1])
+            break
+        default:
+            resultado = "Se necesita una operación valida"
+    }
+} catch(error) {
+    if (error.code==='ECONNREFUSED') {
+        console.log("Hay errores de conexión con la base de datos")
+    }
 }
 
+//Se crea el main para que siempre se ejecute en la consola el resultado
+
 const main = () => {
-    if (resultado=== undefined) {
-      return
+    if (resultado === undefined) {
+        return 
     }
     console.log(resultado)
 }
